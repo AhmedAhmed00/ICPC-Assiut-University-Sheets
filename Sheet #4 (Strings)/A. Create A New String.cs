0f9d51ee847e63cdd8d5cdced1d6c8488197020b
Ahmed Ahmed <116123565+AhmedAhmed00@users.noreply.github.com>
2024-08-03 22:48:43 +0300

    string first

---
+++ Sheet #4 (Strings)/A. Create A New String.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace ProplemSolving { 
+    class Program{ 
+        
+        static void Main(string[] args) {
+
+            string str1;
+            string str2;
+            str1 =  Console.ReadLine();
+           str2 =  Console.ReadLine();
+           Console.WriteLine($"{str1.Length} {str2.Length}\n{str1} {str2}");
+
+        }
+    }
+}
